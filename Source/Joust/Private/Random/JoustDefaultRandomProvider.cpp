b2d9f297c371b4ce2567e65fe55bf9a78e961935
JangJinWoo <aktsksqkq@gmail.com>
2026-08-12 11:30:34 +0900

    3. 랜덤 기능 클래스 구현
    https://app.notion.com/p/3b7547acb1288088a1abd7947cbbe282?source=copy_link#3ba547acb128805685c9d0ecdbf55c3d

---
+++ Source/Joust/Private/Random/JoustDefaultRandomProvider.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Random/JoustDefaultRandomProvider.h"
+
+void UJoustDefaultRandomProvider::Initialize(int32 InSeed)
+{
+	RandomStream.Initialize(InSeed);
+}
+
+int32 UJoustDefaultRandomProvider::GetRandom(int32 Min, int32 Max)
+{
+	return RandomStream.RandRange(Min, Max);
+}
+
+float UJoustDefaultRandomProvider::GetRandom(float Min, float Max)
+{
+	return RandomStream.FRandRange(Min, Max);
+}
+
+FVector2D UJoustDefaultRandomProvider::GetRandom(FVector2D& Min, FVector2D& Max)
+{
+	return FVector2D(
+		RandomStream.FRandRange(Min.X, Max.X), 
+		RandomStream.FRandRange(Min.Y, Max.Y)
+	);
+}
---
+++ Source/Joust/Public/Random/JoustDefaultRandomProvider.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Interface/JoustRandomProvider.h"
+#include "Math/RandomStream.h"
+#include "JoustDefaultRandomProvider.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JOUST_API UJoustDefaultRandomProvider final : public UObject, public IJoustRandomProvider
+{
+	GENERATED_BODY()
+	
+public:
+	/** 지정한 시드로 랜덤 스트림을 초기화 */
+	void Initialize(int32 InSeed);
+
+	// ====================
+	// IJoustRandomProvider 상속됨
+	// ====================
+	int32 GetRandom(int32 Min, int32 Max) override;
+	float GetRandom(float Min, float Max) override;
+	FVector2D GetRandom(FVector2D& Min, FVector2D& Max) override;
+
+private:
+	/** 실제 랜덤 값을 생성하는 Seed 기반 스트림 */
+	FRandomStream RandomStream;
+};
